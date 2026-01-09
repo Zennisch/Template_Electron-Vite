e673fa868a1b1c33a1a7cec80efa4a481481cf15
Zennisch <zennisch@gmail.com>
2026-01-09 19:21:19 +0900

    update: separate create window module

--- src/main/createWindow.ts
+++ src/main/createWindow.ts
@@ -1,4 +1,4 @@
-import { shell, BrowserWindow } from 'electron'
+import { shell, BrowserWindow, WindowOpenHandlerResponse } from 'electron'
 import { join } from 'path'
 import { is } from '@electron-toolkit/utils'
 
@@ -6,6 +6,23 @@ const WINDOW_WIDTH = 1280
 const WINDOW_HEIGHT = 720
 const SHOW_WHEN_CREATED = true
 const ICON = join(__dirname, '../../resources/icon.png')
+
+const onReadyToShow = (window: Electron.BrowserWindow): void => {
+  window.show()
+}
+
+const setWindowOpenHandlerCallback = (details: Electron.HandlerDetails): WindowOpenHandlerResponse => {
+  shell.openExternal(details.url)
+  return { action: 'deny' }
+}
+
+const loadURLOrFile = (window: Electron.BrowserWindow) => {
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    window.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    window.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+}
 
 export const createWindow = (): void => {
   const mainWindow = new BrowserWindow({
@@ -20,18 +37,9 @@ export const createWindow = (): void => {
     }
   })
 
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+  mainWindow.on('ready-to-show', () => onReadyToShow(mainWindow))
 
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
+  mainWindow.webContents.setWindowOpenHandler((details) => setWindowOpenHandlerCallback(details))
 
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-  } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
-  }
+  loadURLOrFile(mainWindow)
 }
