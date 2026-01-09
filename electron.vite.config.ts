8c0fc3a784a1d6a7466f6576c8f601669fe4d1bf
Zennisch <zennisch@gmail.com>
2026-01-09 18:47:03 +0900

    config: electron-vite
    - update: package.json

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,16 +1,37 @@
 import { resolve } from 'path'
 import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  main: {},
-  preload: {},
+  main: {
+    resolve: {
+      alias: {
+        '@main': resolve(__dirname, 'src/main/src'),
+        '@shared': resolve(__dirname, 'src/shared')
+      }
+    }
+  },
+  preload: {
+    resolve: {
+      alias: {
+        '@preload': resolve(__dirname, 'src/preload/src'),
+        '@shared': resolve(__dirname, 'src/shared')
+      }
+    }
+  },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@': resolve(__dirname, 'src/renderer/src'),
+        '@assets': resolve(__dirname, 'src/renderer/src/assets'),
+        '@components': resolve(__dirname, 'src/renderer/src/components'),
+        '@hooks': resolve(__dirname, 'src/renderer/src/hooks'),
+        '@pages': resolve(__dirname, 'src/renderer/src/pages'),
+        '@utils': resolve(__dirname, 'src/renderer/src/utils'),
+        '@shared': resolve(__dirname, 'src/shared')
       }
     },
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
   }
 })
