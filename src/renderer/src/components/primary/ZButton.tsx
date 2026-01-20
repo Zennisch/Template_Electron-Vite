55b4594592b6082d3485e87f69be7f75a52d95e3
Zennisch <zennisch@gmail.com>
2026-01-20 23:23:23 +0900

    update: default spinner icon

--- src/renderer/src/components/primary/ZButton.tsx
+++ src/renderer/src/components/primary/ZButton.tsx
@@ -42,6 +42,14 @@ const sizeClasses: Record<Size, string> = {
   xl: `h-12 text-xl`
 }
 
+const spinnerSizeClasses: Record<Size, string> = {
+  xs: "h-3 w-3",
+  sm: "h-4 w-4",
+  md: "h-5 w-5",
+  lg: "h-6 w-6",
+  xl: "h-7 w-7"
+}
+
 const iconSizeClasses: Record<Size, string> = {
   xs: "w-8 p-0",
   sm: "w-9 p-0",
@@ -220,7 +228,8 @@ const ZButton = forwardRef<HTMLButtonElement, ZButtonProps>((props, ref) => {
     onPointerDown?.(e as any)
   }
 
-  const Spinner = loadingComponent || DefaultSpinnerIcon
+  const Spinner = loadingComponent || <DefaultSpinnerIcon className={cn("animate-spin", spinnerSizeClasses[size])} />
+
   const content =
     loading && loadingText ? (
       <>
--- src/renderer/src/components/primary/utils.tsx
+++ src/renderer/src/components/primary/utils.tsx
@@ -8,15 +8,8 @@ export const cn = (...parts: Array<string | false | undefined>) =>
     .replace(/\s{2,}/g, " ")
     .trim()
 
-export const DefaultSpinnerIcon = (
-  <svg
-    className="animate-spin h-4 w-4"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    role="img"
-    aria-hidden="true"
-  >
+export const DefaultSpinnerIcon = ({ className }: { className?: string }) => (
+  <svg className={className} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" role="img" aria-hidden="true">
     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
   </svg>
